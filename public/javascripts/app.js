9d87452fff9788d95d98e1f05822bd7cc123db52
Steven Abrams <steven.abrams86@gmail.com>
2015-06-10 10:55:41 -0600

    user can see weather for hardcoded location

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,6 +1,16 @@
 var xhr = new XMLHttpRequest;
 xhr.open('get', 'http://www.telize.com/geoip?', true);
 xhr.addEventListener('load', function(){
+
+  var req = new XMLHttpRequest;
+  req.open('get', 'http://api.openweathermap.org/data/2.5/weather?q=boulder,co', true);
+  req.addEventListener('load', function(){
+    var weatherResult = JSON.parse(req.response);
+    var weather = document.getElementsByClassName('weather')[0];
+    weather.innerHTML = weatherResult.weather[0].description;
+  });
+  req.send();
+
   var result = JSON.parse(xhr.response);
   var ip = document.getElementsByClassName('ip')[0];
   var ipContent = ip.innerHTML;
