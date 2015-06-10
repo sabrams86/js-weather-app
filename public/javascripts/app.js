d12970f93b7fe2495535fab4480c8586a50c6156
Steven Abrams <steven.abrams86@gmail.com>
2015-06-10 15:18:34 -0600

    loads weather in users coordinates upon pageload

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2,19 +2,6 @@ var xhr = new XMLHttpRequest;
 xhr.open('get', 'http://www.telize.com/geoip?', true);
 xhr.addEventListener('load', function(){
 
-var button = document.getElementsByClassName('button')[0];
-button.addEventListener('click', function(){
-  event.preventDefault();
-  var location = document.getElementsByClassName('input')[0].value;
-  var req = new XMLHttpRequest;
-  req.open('get', 'http://api.openweathermap.org/data/2.5/weather?q='+location);
-  req.addEventListener('load', function(){
-    var weatherResult = JSON.parse(req.response);
-    var weather = document.getElementsByClassName('weather')[0];
-    weather.innerHTML = weatherResult.weather[0].description;
-  });
-  req.send();
-});
   var result = JSON.parse(xhr.response);
   var ip = document.getElementsByClassName('ip')[0];
   var ipContent = ip.innerHTML;
@@ -27,5 +14,29 @@ button.addEventListener('click', function(){
   var lat = document.getElementsByClassName('lat')[0];
   var latContent = lat.innerHTML;
   lat.innerHTML = latContent+" "+result.latitude+'&deg';
+
+  var xhr2 = new XMLHttpRequest;
+  xhr2.open('get', 'http://api.openweathermap.org/data/2.5/weather?lat='+result.latitude+'&lon='+result.longitude);
+  xhr2.addEventListener('load', function(){
+    var weatherResult = JSON.parse(xhr2.response);
+    var weather = document.getElementsByClassName('weather')[0];
+    weather.innerHTML = weatherResult.weather[0].description;
+  });
+  xhr2.send();
+
+  var button = document.getElementsByClassName('button')[0];
+  button.addEventListener('click', function(){
+    event.preventDefault();
+    var location = document.getElementsByClassName('input')[0].value;
+    var req = new XMLHttpRequest;
+    req.open('get', 'http://api.openweathermap.org/data/2.5/weather?q='+location);
+    req.addEventListener('load', function(){
+      var weatherResult = JSON.parse(req.response);
+      var weather = document.getElementsByClassName('weather')[0];
+      weather.innerHTML = weatherResult.weather[0].description;
+    });
+    req.send();
+  });
+
 });
 xhr.send();
